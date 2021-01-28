82a4b34b69c6749d74e3223b43830a5e04a1c6da
Ron R. Pan <rpan93@gmail.com>
2021-01-27 23:01:17 -0500

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,6 +1,6 @@
-docker build --no-cache -t emailtovamos/dockerk8swalkthrough:v1 .
+docker build --no-cache -t rpan/dockerk8swalkthrough:v1 .
 
-docker push emailtovamos/dockerk8swalkthrough:v1
+docker push rpan/dockerk8swalkthrough:v1
 
 kubectl delete deployment dockerk8swalkthrough
 
